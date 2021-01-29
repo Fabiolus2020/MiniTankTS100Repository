0f5199476c02473da6840c4c5051c2ea3b8979a0
Fabiolus2020 <76980761+Fabiolus2020@users.noreply.github.com>
2021-01-28 20:32:51 -0500

    Update receiverVERSION2.ino

--- receiverVERSION2.ino
+++ receiverVERSION2.ino
@@ -1,48 +1,51 @@
-//Include Libraries
+// Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
 RF24 myRadio(8, 9); // CE, CSN
-struct package
-{
-  int motorspeed1;
-  int motorspeed2;
-  int motordirection;
-};
 
 //address through which two modules communicate.
 const byte address[6] = "00001";
 
-//byte addresses[][6] = {"0"};
 
-// Motor A Connections
-int enA = 10;
-int in1 = 4;
-int in2 = 5;
-
-// Motor B Connections
-int enB = 3;
-int in3 = 6;
-int in4 = 7;
+struct package
+{
+  int joyposX;
+  int joyposY;
+  int potValue;
+  int modeNow;
+};
 
 typedef struct package Package;
 Package data;
 
-void setup()
-{
-  while (!Serial);
+#define enA 10   // Note: Pin 9 in previous video ( pin 10 is used for the SPI communication of the NRF24L01)
+#define in1 4
+#define in2 5
+#define enB 3   // Note:  Pin 10 in previous video
+#define in3 6
+#define in4 7
+
+
+
+int motorSpeedA = 0;
+int motorSpeedB = 0;
+
+void setup() {
+
+ while (!Serial);
+
   Serial.begin(9600);
   myRadio.begin();
   myRadio.setChannel(115);
   myRadio.setPALevel(RF24_PA_MAX);
   myRadio.setDataRate( RF24_250KBPS ) ;
-  //myRadio.openReadingPipe(1, addresses[0]);
 
   //set the address
   myRadio.openReadingPipe(0, address);
+
   myRadio.startListening();
-  // Set all the motor control pins to outputs
   pinMode(enA, OUTPUT);
   pinMode(enB, OUTPUT);
   pinMode(in1, OUTPUT);
@@ -52,63 +55,89 @@ void setup()
 
 }
 
-  void loop()
+
+void loop() {
+  if ( myRadio.available())
   {
-    if ( myRadio.available())
-    {
-      //while (myRadio.available())
-      //{
-        myRadio.read( &data, sizeof(data) );
-      //}
 
-      //Serial Print the values
-     // Serial.print(": motorspeed1 :");
-    //  Serial.println(data.motorspeed1);
-    //  Serial.print(" motorspeed2 :");
-    //  Serial.println(data.motorspeed2);
-    //  Serial.print(" direction");
-     // Serial.println(data.motordirection);
-      //delay (2000);
-      // Set Motor Direction
-      if (data.motordirection == 1)
-      {
-        // Motors are backwards
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-      }
+      myRadio.read( &data, sizeof(data) );
+   
+   // Serial.print("X : ");
+   // Serial.println(data.joyposX);
+   // Serial.print("Y : ");
+  //  Serial.println(data.joyposY);
 
-      if (data.motordirection == 0)
-      {
-        // Motors are forwards
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-      }
 
-      if (data.motordirection == 3)
-      {
-        // Motors are forwards
-        digitalWrite(in1, HIGH);
-        digitalWrite(in2, LOW);
-        digitalWrite(in3, LOW);
-        digitalWrite(in4, HIGH);
-      }
 
-      if (data.motordirection == 4)
-      {
-        // Motors are forwards
-        digitalWrite(in1, LOW);
-        digitalWrite(in2, HIGH);
-        digitalWrite(in3, HIGH);
-        digitalWrite(in4, LOW);
-      }
-      // Drive Motors
-      analogWrite(enA, data.motorspeed1);
-      analogWrite(enB, data.motorspeed2);
-
+    // X-axis used for forward and backward control
+    if (data.joyposX < 470) {
+      // Set Motor A backward
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      // Set Motor B backward
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+      // Convert the declining Y-axis readings for going backward from 470 to 0 into 0 to 255 value for the PWM signal for increasing the motor speed
+      motorSpeedA = map(data.joyposX, 470, 0, 0, 255);
+      motorSpeedB = map(data.joyposX, 470, 0, 0, 255);
     }
-    delay(30);
+    else if (data.joyposX > 550) {
+      // Set Motor A forward
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      // Set Motor B forward
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, HIGH);
+      // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
+      motorSpeedA = map(data.joyposX, 550, 1023, 0, 255);
+      motorSpeedB = map(data.joyposX, 550, 1023, 0, 255);
+    }
+    // If joystick stays in middle the motors are not moving
+    else {
+      motorSpeedA = 0;
+      motorSpeedB = 0;
+    }
+
+
+    // Y-axis used for left and right control
+    //this is left
+    if (data.joyposY < 470) {
+
+      // Set Motor A forward
+      digitalWrite(in1, LOW);
+      digitalWrite(in2, HIGH);
+      // Set Motor B backward
+      digitalWrite(in3, HIGH);
+      digitalWrite(in4, LOW);
+
+      // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
+      motorSpeedA = map(data.joyposY, 470, 0, 0, 255);
+      motorSpeedB = map(data.joyposY, 470, 0, 0, 255);
+    }
+
+
+    if (data.joyposY > 550) {
+
+      // Set Motor A forward
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, LOW);
+      // Set Motor B backward
+      digitalWrite(in3, LOW);
+      digitalWrite(in4, HIGH);
+
+      // Convert the increasing Y-axis readings for going forward from 550 to 1023 into 0 to 255 value for the PWM signal for increasing the motor speed
+      motorSpeedA = map(data.joyposY, 550, 1023, 0, 255);
+      motorSpeedB = map(data.joyposY, 550, 1023, 0, 255);
+    }
+    // Prevent buzzing at low speeds (Adjust according to your motors. My motors couldn't start moving if PWM value was below value of 70)
+    if (motorSpeedA < 70) {
+      motorSpeedA = 0;
+    }
+    if (motorSpeedB < 70) {
+      motorSpeedB = 0;
+    }
+    analogWrite(enA, motorSpeedA); // Send PWM signal to motor A
+    analogWrite(enB, motorSpeedB); // Send PWM signal to motor B
   }
+
+}
