1b8da00a31732972da9bbc7a35627fbe75d66705
Fabiolus2020 <76980761+Fabiolus2020@users.noreply.github.com>
2021-01-28 20:27:02 -0500

    Update controllerVERSION1.ino

--- controllerVERSION1.ino
+++ controllerVERSION1.ino
@@ -1,26 +1,25 @@
-// Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
-
+//Include Libraries
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+
 RF24 myRadio(8, 9); // CE, CSN
 
 //address through which two modules communicate.
 const byte address[6] = "00001";
 
-
-
-
+//byte addresses[][6] = {"0"};
 
 struct package
 {
   int joyposX;
   int joyposY;
+  int potValue;
+  int modeNow;
 };
 
 typedef struct package Package;
 Package data;
-
 // Define Joystick Connections
 #define joyX A0
 #define joyY A1
@@ -29,31 +28,44 @@ Package data;
 int joyposX = 512;
 int joyposY = 512;
 
-void setup() {
+void setup()
+{
+
   Serial.begin(9600);
+  delay(100);
   myRadio.begin();
   myRadio.setChannel(115);
+  myRadio.setPALevel(RF24_PA_MIN);
+  myRadio.setDataRate( RF24_250KBPS );
 
+ //myRadio.openWritingPipe( addresses[0]);
  //set the address
- myRadio.openWritingPipe(address);
- myRadio.setPALevel(RF24_PA_MAX);
- myRadio.setDataRate( RF24_250KBPS );
+  myRadio.openWritingPipe(address);
 
 //Set module as transmitter
-myRadio.stopListening();
+  myRadio.stopListening();
+
 
   delay(100);
+
+
+
 }
-void loop() {
+
+void loop()
+{
+
   myRadio.write(&data, sizeof(data));
-  
+  // Print to Serial Monitor
+  // Serial.println("Reading motorcontrol values ");
+
+  // Read the Joystick X and Y positions
   data.joyposX = analogRead(joyX);
   data.joyposY = analogRead(joyY);
 
-//Serial.print("xAxis :");
-//Serial.println(data.joyposX);
-//Serial.print("yAxis :");
-//Serial.println(data.joyposY);
-  delay(20);
+ 
+ 
 
+
+  delay(100);  // Wait a bit before next transmission
 }
